f9729f47191c714becb1f6e08507e86bb702ed40
lukasbahr <lukas.bahr@rwth-aachen.de>
2019-09-06 11:01:53 +0200

    fixed evaluation of fixed z

--- lib/evaluation.py
+++ lib/evaluation.py
@@ -4,6 +4,7 @@ import torch
 from torchvision.utils import save_image
 
 import numpy as np
+from scipy.stats import norm
 
 
 def save_recon_images(args, model, validation_loader, data_shape):
@@ -56,33 +57,54 @@ def save_recon_images(args, model, validation_loader, data_shape):
 def save_fixed_z_image(args, model, data_shape):
     """ Save samples with fixed z. """
 
+    number_img = 100
+
     with torch.no_grad():
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
         cvt = lambda x: x.type(torch.float32).to(device, non_blocking=True)
-        fixed_z = cvt(torch.randn(100, *data_shape))
 
         if args.model == "ffjord":
+            fixed_z = cvt(torch.randn(number_img, *data_shape))
             generated_sample = model(fixed_z, reverse=True)
+
         elif args.model == "snf":
-            generated_sample, z_mu, z_var, ldj, z0, z_k = model(fixed_z)
+            fixed_z = cvt(torch.randn(number_img, args.z_size))
+            generated_sample = model.decode(fixed_z)
 
         if args.data == "piv":
-            x_cat_1 = torch.cat([generated_sample[:args.save_recon_images_size,0,:,:]],0).view(-1, 1, data_shape[1], data_shape[2])
-            x_cat_2 = torch.cat([generated_sample[:args.save_recon_images_size,1,:,:]],0).view(-1, 1, data_shape[1], data_shape[2])
+            x_cat_1 = torch.cat([generated_sample[:number_img, 0,:,:]],0).view(-1, 1, data_shape[1], data_shape[2])
+            x_cat_2 = torch.cat([generated_sample[:number_img, 1,:,:]],0).view(-1, 1, data_shape[1], data_shape[2])
 
             images = [x_cat_1.cpu().data,x_cat_2.cpu().data]
 
             count = 0
             for image in images:
                 name = args.save + '/fixed_z_' + args.experiment_name + str(count) + '.png'
-                save_image(image, name, nrow=8 )
+                save_image(image, name, nrow=10 )
                 count += 1
 
         else:
             x_cat = torch.cat([generated_sample[:args.save_recon_images_size,0,:,:]],
                     0).view(-1, 1, data_shape[1], data_shape[2])
             name = args.save + '/fixed_z_' + args.experiment_name + '.png'
-            save_image(x_cat, name, nrow=8)
+            save_image(x_cat, name, nrow=10)
+
+
+def save_2D_manifold(args, model, data_shape):
+
+    n = 15
+    digit_size = data_shape[1]
+    figure = np.zeros((digit_size*n, digit_size*n))
+
+    grid_x = norm.ppf(np.linspace(0.05,0.95, n))
+    grid_y = norm.ppf(np.linspace(0.05,0.95, n))
+
+    for i, yi in enumerate(grid_x):
+        for j, xi in enumerate(grid_y):
+            z_sample = np.array([xi, yi])
+            z_sample = torch.Tensor())
+
 
 
